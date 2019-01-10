04808567bedab03d5fe3d8947a48f8c1b77b3276
Fermin Lopez <fermin.lopez96@gmail.com>
2019-01-10 09:19:29 -0600

    ready for deploy

--- LangEx/server.js
+++ LangEx/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
@@ -34,6 +35,13 @@ app.get('/', (req,res) => {
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000;
 console.log(port)
